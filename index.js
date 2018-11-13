61e846fcec3efdbddbf06ddd0c68cadb1f80b3fd
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-13 21:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+function specialGreeting () {
+  return "Hello, " + specialGuest + "!"
+}
+
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
